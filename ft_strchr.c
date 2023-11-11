12afcb4f10449d453c8b584734744fad555781ef
pibosc <pibosc@student.42.fr>
2023-11-11 16:44:44 +0100

    fixed strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 18:00:07 by pibosc            #+#    #+#             */
-/*   Updated: 2023/11/09 19:27:45 by pibosc           ###   ########.fr       */
+/*   Updated: 2023/11/11 16:44:27 by pibosc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	while (s[i] && s[i] != c % 256)
+	while (s[i] && s[i] != (unsigned char) c)
 		i++;
-	if (s[i] == c % 256)
+	if (s[i] == (unsigned char) c)
 		return ((char *)(s + i));
 	if (c == 0)
 		return ((char *)s);
