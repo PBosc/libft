17d9c260e3601a87de15da8b56fab531a1bf5ddd
Pierre Bosc <pibosc@made-f0Ar11s6.clusters.42paris.fr>
2023-11-13 20:33:22 +0100

    memchr fixed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 18:13:40 by pibosc            #+#    #+#             */
-/*   Updated: 2023/11/12 18:18:35 by pibosc           ###   ########.fr       */
+/*   Updated: 2023/11/13 20:31:34 by pibosc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n && *(unsigned char *)(s + i) != (unsigned char)c
-		&& *(unsigned char *)(s + i))
-		i++;
-	if (*(unsigned char *)(s + i) == (unsigned char)c && i < n)
-		return ((void *)(s + i));
+	while (n--)
+	{
+		if (*((unsigned char *) s) == (unsigned char) c)
+			return ((void *) s);
+		s++;
+	}
 	return (NULL);
 }
